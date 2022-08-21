938c4064aea64b5f29b8382112edf57d560fda5f
ntpadovani <normant_padovani@hotmail.com>
2022-08-21 12:55:18 -0400

    Changed the separate function to not allocate with malloc on the darray iteration.

--- separate.c
+++ separate.c
@@ -16,7 +16,6 @@ char **separate(char *string, const char *s)
 	size = count_words(string);
 	printf("Size is:%d\n", size);
 	darray = malloc(sizeof(char *) * size + 1);
-
 	if (darray == NULL)
 	{
 		return NULL;
@@ -24,13 +23,14 @@ char **separate(char *string, const char *s)
 	tokenized = strtok(string, s);
 	while (tokenized != NULL)
 	{
-		darray[idx] = malloc(sizeof(char) * _strlen(tokenized) + 1);
+		/*darray[idx] = malloc(sizeof(char) * _strlen(tokenized) + 1);
 		if (darray[idx] == NULL)
 		{
 			free(darray);
 			return (NULL);
-		}
+		}*/
 		darray[idx] = tokenized;
+
 		idx++;
 		tokenized = NULL;
 		tokenized = strtok(NULL, s);
