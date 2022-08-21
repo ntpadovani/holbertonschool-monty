6e70faaf3812901dc2c6fa8649a3f1d886eb8001
Ivanzkaa <ivanzka@me.com>
2022-08-21 19:24:59 -0400

    fixing a betty issue

--- pall.c
+++ pall.c
@@ -2,19 +2,19 @@
 
 /**
  * pall - a function that prints all the elements of a linked list.
- * @h: pointer to first element of the linked list.
- *
+ *	@head: head pointer
+ *	@line_number: function used to count the lines
  * Return: the contents of all the nodes in a linked list.
  */
 
-void pall(stack_t **head, __attribute__((unused))unsigned int line_number)
+void pall(stack_t **head, __attribute__((unused)) unsigned int line_number)
 {
-	const stack_t *ptr = *head;/*apunto el pointer al head pointer*/
+	const stack_t *ptr = *head; /*apunto el pointer al head pointer*/
 
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		printf("%d\n", (*ptr).n);
-		ptr = (*ptr).next;/*para correr de nodo en nodo*/
+		ptr = (*ptr).next; /*para correr de nodo en nodo*/
 	}
-	return;
+	exit(EXIT_SUCCESS);
 }
